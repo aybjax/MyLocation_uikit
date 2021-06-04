3837440154dc50ae03388069700546d79da2d1a2
aybjax <aybat93@gmail.com>
2021-06-05 01:19:19 +0600

    map pin annotations

--- MyLocations/SceneDelegate.swift
+++ MyLocations/SceneDelegate.swift
@@ -53,10 +53,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             let controller1 = navController.viewControllers.first as! CurrentLocationViewController
             controller1.managedObjectContext = managedObjectContext
             
-            //Second tab
+            // Second tab
             navController = tabViewControllers[1] as! UINavigationController
             let controller2 = navController.viewControllers.first as! LocationsViewController
             controller2.managedObjectContext = managedObjectContext
+            
+            // Third tab
+            navController = tabViewControllers[2] as! UINavigationController
+            let controller3 = navController.viewControllers.first as! MapViewController
+            controller3.managedObjectContext = managedObjectContext
         }
         
         listenForFatalCoreDataNotifications()
